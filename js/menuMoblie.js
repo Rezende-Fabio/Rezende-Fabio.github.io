48d6df7e8eabc57a54603d3f60d6ba31eb7b972e
Rezende-Fabio <rezendefabio337@gmail.com>
2024-02-24 18:25:11 -0300

    added skill section

--- js/menuMoblie.js
+++ js/menuMoblie.js
@@ -1,11 +1,18 @@
 class MenuMobile{
-    constructor(menuMobile, navList, navLinks){
+    constructor(menuMobile, navList, navLinks, alinks){
         this.menuMobile = document.querySelector(menuMobile);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
+        this.links = document.querySelectorAll(alinks);
         this.classeAtiva = "ativa";
 
         this.handleClick = this.handleClick.bind(this);
+    }
+
+    addClickEventLink(){
+        this.links.forEach((link) => {
+            link.addEventListener("click", this.handleClick);
+        });
     }
 
     animateLinks(){
@@ -29,6 +36,7 @@ class MenuMobile{
     init(){
         if (this.menuMobile){
             this.addClickEvent();
+            this.addClickEventLink();
         }
         
         return this;
@@ -38,7 +46,8 @@ class MenuMobile{
 const menuMobile = new MenuMobile(
     ".menu-mobile",
     ".nav-list",
-    ".nav-list li"
+    ".nav-list li",
+    ".link"
 );
 
 menuMobile.init();
