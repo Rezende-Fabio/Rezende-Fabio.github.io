505fcd8adf376c06fe1e554a2002ffb6e9ec611e
Rezende-Fabio <rezendefabio337@gmail.com>
2024-05-22 21:24:19 -0300

    FEAT: added animation meteors

--- js/animation.js
+++ js/animation.js
@@ -18,3 +18,42 @@ animeScroll();
 window.addEventListener("scroll", () => {
     animeScroll();
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const meteorContainer = document.querySelector('.meteors');
+    let activeMeteors = 0;
+    const maxMeteors = 20;
+
+    function createMeteor() {
+        if (activeMeteors >= maxMeteors) {
+            return;
+        }
+
+        const meteor = document.createElement('div');
+        meteor.classList.add('meteor');
+
+        meteor.style.top = `${-100}px`;
+        meteor.style.left = `${Math.random() * -100}vw`;
+
+        meteor.style.animation = `moveMeteor ${Math.random() * 2 + 3}s linear`;
+
+        meteorContainer.appendChild(meteor);
+        activeMeteors++;
+
+        meteor.addEventListener('animationend', () => {
+            meteor.remove();
+            activeMeteors--;
+        });
+    }
+
+    function createMeteorShower() {
+        for (let i = 0; i < 50; i++) {
+            createMeteor();
+        }
+    }
+
+    setInterval(createMeteorShower, 30000); 
+
+    setInterval(createMeteor, 3000);
+});
